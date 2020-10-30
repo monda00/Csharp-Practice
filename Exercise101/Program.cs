e5c0e2f13c8486f214900e3f56a826f4952a5e7d
monda00 <monda0524@gmail.com>
2020-10-30 23:43:38 +0900

    add Exercise101

---
+++ Exercise101/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hoge");
+            Console.WriteLine("hoge");
+            Console.WriteLine("hoge");
+        }
+    }
+}
